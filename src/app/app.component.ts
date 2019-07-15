574a8e57ea7d4901c864d51efa91b0ddec95a9fc
Goplani <aakash.goplani@accenture.com>
2019-07-15 16:41:52 +0530

    resolve merge conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./app.component.css']
-=======
   // styleUrls: ['./app.component.css'],
   styles: [`h1 { text-align: center;}`]
->>>>>>> origin/master
 })
 export class AppComponent {
   title = 'angular-basics';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-
-@NgModule({
-  declarations: [
-    AppComponent
-=======
 import { ServerComponent } from './server/server.component';
 import { NestedServersComponent } from './nested-servers/nested-servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
@@ -20,7 +14,6 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     NestedServersComponent,
     WarningAlertComponent,
     SuccessAlertComponent
->>>>>>> origin/master
   ],
   imports: [
     BrowserModule
