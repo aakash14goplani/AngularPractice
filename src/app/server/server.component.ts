a96b92c101e5b6b15e0973c809ea63f204637c52
Goplani <aakash.goplani@accenture.com>
2019-07-19 21:00:04 +0530

    wrap up for the day

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -13,10 +13,20 @@ export class ServerComponent {
     width: number = 230;
     height: number = 230;
     align: string = 'left';
+    someString: string = '';
 
     onClickChangeAlignment() {
         let align: string[] = ['top', 'middle', 'bottom', 'left', 'right'];
         let index: number = Math.floor(Math.random() * 4);
         return this.align = align[index];
     }
+
+    printData(event: Event) {
+        return this.someString = (<HTMLInputElement>event.target).value;
+    }
+/* notes about casting, error faced, $event. Two-Way-Binding (covered in the next lecture) to work, you need to enable the ngModel  directive. This is done by adding the FormsModule  to the imports[]  array in the AppModule.
+
+You then also need to add the import from @angular/forms  in the app.module.ts file:
+
+import { FormsModule } from '@angular/forms'; */
 }
