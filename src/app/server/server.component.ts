a1c8d0b4d8531b717169c7bf56c566a37614dce5
Goplani <aakash.goplani@accenture.com>
2019-07-19 18:19:38 +0530

    working with evenet binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -12,4 +12,11 @@ export class ServerComponent {
     imageURL: string = 'https://avatars3.githubusercontent.com/u/13452115?s=460&v=4';
     width: number = 230;
     height: number = 230;
+    align: string = 'left';
+
+    onClickChangeAlignment() {
+        let align: string[] = ['top', 'middle', 'bottom', 'left', 'right'];
+        let index: number = Math.floor(Math.random() * 4);
+        return this.align = align[index];
+    }
 }
