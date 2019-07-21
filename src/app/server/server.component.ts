7f18f8b7020113cf4331571c9eac4f1dc0408111
aakash14goplani <goplaniaakash14@gmail.com>
2019-07-21 23:05:28 +0530

    updated code for components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,27 +6,39 @@ import {Component} from '@angular/core';
     styles: [`
         h3 { color:red; }
         h6 { color:blue; }
+        .margin-20-top-bottom { margin: 20px 0;}
     `]
 })
 export class ServerComponent {
-    imageURL: string = 'https://avatars3.githubusercontent.com/u/13452115?s=460&v=4';
+    imageURL: string = 'https://avatars3.githubusercontent.com/u/13452115';
     width: number = 230;
     height: number = 230;
     align: string = 'left';
     someString: string = '';
+    username: string = '';
 
     onClickChangeAlignment() {
-        let align: string[] = ['top', 'middle', 'bottom', 'left', 'right'];
-        let index: number = Math.floor(Math.random() * 4);
+        const align: string[] = ['top', 'middle', 'bottom', 'left', 'right'];
+        const index: number = Math.floor(Math.random() * 4);
         return this.align = align[index];
     }
 
     printData(event: Event) {
-        return this.someString = (<HTMLInputElement>event.target).value;
+        // console.log(event);
+        return this.someString = ( event.target as HTMLInputElement).value;
     }
-/* notes about casting, error faced, $event. Two-Way-Binding (covered in the next lecture) to work, you need to enable the ngModel  directive. This is done by adding the FormsModule  to the imports[]  array in the AppModule.
 
-You then also need to add the import from @angular/forms  in the app.module.ts file:
+    isUsernameEmpty() {
+        if (this.username.length > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-import { FormsModule } from '@angular/forms'; */
+    deleteUsername() {
+        return this.username = '';
+    }
+// tslint:disable-next-line: max-line-length
+/* error faced. Event-bubbling */
 }
