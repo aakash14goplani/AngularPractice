795b19fa4881a486a13dff90435f259a5b987ab9
aakash14goplani <goplaniaakash14@gmail.com>
2019-07-22 02:18:47 +0530

    updated code for directives

--- src/app/nested-servers/nested-servers.component.ts
+++ src/app/nested-servers/nested-servers.component.ts
@@ -8,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NestedServersComponent  {
   serverId: number = 10;
-  serverStatus: string[] = ['online', 'offline'];
+  serverStatus: string = '';
+  allowNewServer: boolean = false;
+  serverCreated: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = 'Test_Server_';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
 
   getServerStatus() {
-    return this.serverStatus[this.serverId % 2];
+    return this.serverStatus; // one time initialization in constructor to avoid error below
+    // return Math.random() > 0.5 ? 'Online' : 'Offline'; => Error: ExpressionChangedAfterItHasBeenCheckedError
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
   }
 
 }
