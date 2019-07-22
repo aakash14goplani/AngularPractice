2e67233c53a287554401661900423edf24118c08
aakash14goplani <goplaniaakash14@gmail.com>
2019-07-23 01:44:27 +0530

    added for & switch

--- src/app/nested-servers/nested-servers.component.ts
+++ src/app/nested-servers/nested-servers.component.ts
@@ -13,6 +13,12 @@ export class NestedServersComponent  {
   serverCreated: boolean = false;
   serverCreationStatus: string = 'No server was created!';
   serverName: string = 'Test_Server_';
+  serversCollection: any[] = [
+    {id: 1, name: 'Server 1'},
+    {id: 2, name: 'Server 2'},
+    {id: 3, name: 'Server 3'},
+    {id: 4, name: 'Server 4'},
+  ];
 
   constructor() {
     setTimeout(() => {
@@ -38,6 +44,11 @@ export class NestedServersComponent  {
 
   getColor() {
     return this.serverStatus === 'Online' ? 'green' : 'red';
+  }
+
+  addServerInstance() {
+    this.serversCollection.push({id: this.serverId, name: this.serverName});
+    // return '';
   }
 
 }
