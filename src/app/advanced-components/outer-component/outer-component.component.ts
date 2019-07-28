077133cce5e8eb552f9087100e076b5f4d6aadf4
aakash14goplani <goplaniaakash14@gmail.com>
2019-07-29 01:49:28 +0530

    added notes for input section

--- src/app/advanced-components/outer-component/outer-component.component.ts
+++ src/app/advanced-components/outer-component/outer-component.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-outer-component',
   templateUrl: './outer-component.component.html',
-  styleUrls: ['./outer-component.component.css']
+  styleUrls: ['./outer-component.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class OuterComponentComponent implements OnInit {
 
   serverName: string = '';
   serverContents: string = '';
+  templateReferenceValue: string = '';
+  @ViewChild('viewChild', {static: false}) viewChildReferenceValue: ElementRef;
+  tempVar = '';
   // @Output() contentCreated = new EventEmitter<{type: string, serverName: string, serverContents: string}>();
   // tslint:disable-next-line: no-output-rename
   @Output('customElement') contentCreated = new EventEmitter<{type: string, serverName: string, serverContents: string}>();
@@ -24,5 +28,13 @@ export class OuterComponentComponent implements OnInit {
       serverName: this.serverName,
       serverContents: this.serverContents
     });
+  }
+
+  testTemplateReference(templateReference: HTMLInputElement) {
+    this.templateReferenceValue = templateReference.value;
+  }
+
+  viewChildReference() {
+    this.tempVar = this.viewChildReferenceValue.nativeElement.value;
   }
 }
