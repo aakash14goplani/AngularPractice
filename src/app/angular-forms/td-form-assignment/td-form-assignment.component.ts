4657d2761ebeee713ca4abb074e338dcda7cf995
Goplani <aakash.goplani@accenture.com>
2019-09-06 13:32:35 +0530

    completed TD assignment

--- src/app/angular-forms/td-form-assignment/td-form-assignment.component.ts
+++ src/app/angular-forms/td-form-assignment/td-form-assignment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-td-form-assignment',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TdFormAssignmentComponent implements OnInit {
 
+  subscription: string[] = ['Basic', 'Advance', 'PRO'];
+  defaultSubscription: string = 'Basic';
+  @ViewChild('form', {static: false}) formData: NgForm;
+  formSubmitted: boolean = false;
+  assignment = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onSubmit(): void {
+    console.log('formData: ', this.formData);
+    this.formSubmitted = true;
+
+    this.assignment.email = this.formData.value.email;
+    this.assignment.subscription = this.formData.value.subscription;
+    this.assignment.password = this.formData.value.password;
+  }
+
+  onReset(): void {
+    this.formData.reset();
   }
 
 }
